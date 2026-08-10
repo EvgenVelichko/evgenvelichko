25cc5fc627ddca6c531f77a93eb0ea06cea08f61
evgenvelychko <evgenvelicko4@gmail.com>
2026-08-10 19:46:43 +0300

    Improve mobile reveal animations: earlier trigger and faster transition on small screens

--- src/hooks/useReveal.ts
+++ src/hooks/useReveal.ts
@@ -14,7 +14,7 @@ const scan = () => {
           }
         });
       },
-      { threshold: 0.12, rootMargin: '0px 0px -40px 0px' },
+      { threshold: 0.05, rootMargin: '0px 0px -10px 0px' },
     );
   }
   document
